3d1bdd9edc5c7de1f3c3d365f15fbe44e5932b91
bud <bud@bud-laptop.(none)>
2011-01-25 23:17:45 -0500

    Addition of the fiducials

---
+++ FiducialClass.h
@@ -0,0 +1,37 @@
+#ifndef _FiducialClass_h
+#define _FiducialClass_h
+
+#include "ColorableClass.h"
+
+class FiducialClass : public ColorableClass
+{
+public:
+
+  FiducialClass();
+  std::string GetType() ;
+  void Print() ;
+  void SetId( std::string ) ;
+  std::string GetId() ;
+  void SetLabelText( std::string ) ;
+  std::string GetLabel() ;
+  void Setxyz( float , float , float ) ;
+  std::vector< float > Getxyz() ;
+  void SetOrientation( float , float , float , float ) ;
+  std::vector< float > GetOrientation() ;
+  void Selected( bool ) ;
+  bool IsSelected() ;
+  void Visibility( bool ) ;
+  bool IsVisible() ;
+  void SetTextScale( double ) ;
+  double GetTextScale() ;
+private:
+  std::string m_Id ;
+  std::string m_Label ;
+  float m_X, m_Y, m_Z ;
+  float m_oW, m_oX, m_oY, m_oZ ;
+  bool m_Selected ;
+  bool m_Visible ;
+  double m_TextScale ;
+};
+
+#endif
